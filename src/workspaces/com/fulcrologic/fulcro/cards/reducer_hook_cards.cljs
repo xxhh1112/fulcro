a6a92155f62774987ecca925bb55b3ff091af0b2
Tony Kay <tony.kay@gmail.com>
2020-06-04 21:22:18 -0700

    beefed up example to be more interesting

--- src/workspaces/com/fulcrologic/fulcro/cards/reducer_hook_cards.cljs
+++ src/workspaces/com/fulcrologic/fulcro/cards/reducer_hook_cards.cljs
@@ -6,9 +6,12 @@
     [com.fulcrologic.fulcro.components :as comp :refer [defsc]]
     [com.fulcrologic.fulcro.application :as app]
     [com.fulcrologic.fulcro.dom :as dom]
-    [com.fulcrologic.fulcro.mutations :as m]
+    [com.fulcrologic.fulcro.mutations :as m :refer [defmutation]]
     [taoensso.timbre :as log]
-    [com.fulcrologic.fulcro.react.hooks :as hooks]))
+    [com.fulcrologic.fulcro.react.hooks :as hooks]
+    [com.fulcrologic.fulcro.algorithms.merge :as merge]))
+
+(declare Top)
 
 ;; Map from prop -> reducer that can generate that prop from state
 (defonce reducers (atom {}))
@@ -22,9 +25,13 @@
   (swap! reducers assoc prop-key reducer))
 
 ;; Side effect to add a reducer for a given generated prop
-(register-reducer! :generated/data (fn [app {:keys [after]}]
-                                     (+ (get-in after [:component/id ::top :x])
-                                       (get-in after [:component/id ::top :y]))))
+(register-reducer! :all-known-sales/total (fn [app {:keys [after]}]
+                                            (let [sales (vals (:sale/id after))]
+                                              (reduce
+                                                (fn [acc {:sale/keys [amount]}]
+                                                  (+ acc amount))
+                                                0
+                                                sales))))
 
 (defn reducer-tx-hook
   "A transaction hook that looks at mounted components to see what registered props need to be re-generated, and then
@@ -57,26 +64,48 @@
           state-map
           (keys generated-data))))))
 
+(defsc Sale [this {:sale/keys [amount]}]
+  {:query         [:sale/id :sale/amount]
+   :ident         :sale/id
+   :initial-state {:sale/id :param/id :sale/amount :param/amount}}
+  (dom/div "Sale: " amount))
+
+(def ui-sale (comp/factory Sale))
+
 (defsc Child [this {:generated/keys [data]
-                    :child/keys     [id name] :as props}]
-  {:query         [:child/id :child/name :generated/data]
+                    :child/keys     [id name sales] :as props}]
+  {:query         [:child/id :child/name {:child/sales (comp/get-query Sale)}
+                   :generated/data]
    :ident         :child/id
-   :initial-state {:child/id :param/id :child/name :param/name}}
+   :initial-state {:child/id    :param/id
+                   :child/sales :param/sales
+                   :child/name  :param/name}}
   (dom/div
-    (dom/p "Name:" name)
-    (dom/p "Data:" data)))
+    (dom/p "Name:" name "with generated data" data)
+    (dom/ul
+      (map ui-sale sales))))
 
 (def ui-child (comp/factory Child {:keyfn :child/id}))
 
-(defsc Top [this {:keys [x children generated/data] :as props}]
-  {:query         [:x :y {:children (comp/get-query Child)} :generated/data]
+(defn add-sale* [state-map child-id amount]
+  (merge/merge-component state-map Sale {:sale/id (random-uuid) :sale/amount amount} :append [:child/id child-id :child/sales]))
+
+(defmutation new-sale [{:keys [amount]}]
+  (action [{:keys [state]}]
+    (swap! state add-sale* 2 amount)))
+
+(defsc Top [this {:keys [x children all-known-sales/total] :as props}]
+  {:query         [:x {:children (comp/get-query Child)} :all-known-sales/total]
    :ident         (fn [] [:component/id ::top])
    :initial-state {:x        1
-                   :y        3
-                   :children [{:id 1 :name "Joe"}]}}
+                   :children [{:id 1 :name "Joe" :sales [{:id 1 :amount 11.95}]}
+                              {:id 2 :name "Sam" :sales [{:id 2 :amount 10.05}]}
+                              {:id 3 :name "Sally" :sales [{:id 3 :amount 2.05} {:id 4 :amount 2.45}]}
+                              {:id 4 :name "Barbara" :sales []}]}}
   (dom/div
-    (dom/p "Generated data: " data)
-    (dom/button {:onClick (fn [] (m/set-integer! this :x :value (inc x)))} "Bump x")
+    (dom/p "All known sales total: " total)
+    (dom/button {:onClick (fn [] (m/set-integer! this :x :value (inc x)))} (str "Bump x " x))
+    (dom/button {:onClick (fn [] (comp/transact! this [(new-sale {:amount (rand-int 10)})]))} "Sell something!")
     (mapv ui-child children)))
 
 (ws/defcard floating-reducer-hook-card
